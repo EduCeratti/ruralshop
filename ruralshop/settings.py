36273171d52b8a036b7276b009b0ae23b3c72e00
ceratti <eduardo.ceratti@corp.terra.com>
2018-06-13 14:21:44 -0300

    commit no new branch

--- ruralshop/settings.py
+++ ruralshop/settings.py
@@ -24,6 +24,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
+# Alteração Eduardo Ceratti
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
